b98e55e0f223454d8904b04739d74bd0953ba78d
Ilyas Munzir <smunzir@gmail.com>
2020-09-21 16:00:00 -0500

    Initial Commit

---
+++ Multiply/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiply
+//
+//  Created by Student on 9/21/20.
+//  Copyright © 2020 smunzir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
